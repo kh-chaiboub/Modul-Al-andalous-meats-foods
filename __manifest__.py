ccff9ca3c64a1f0b6ff56110fb46d0ef18717b16
CHAIBOUB <chaiboub@gr.ma>
2022-09-21 13:29:19 +0100

    First commit with creation modul Al andalous

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Al_andalous',
+    'version' : '1.1',
+    'summary': 'Summary',
+    'author': 'Khalid chaiboub Works',
+    'sequence': 1,
+    'description': """
+Description
+====================
+description
+    """,
+    'category': 'autre',
+    'website': 'https://www.alandalous.ma',
+    'depends' : ['base'],
+    
+    'data': ['data/data.xml',
+        
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
